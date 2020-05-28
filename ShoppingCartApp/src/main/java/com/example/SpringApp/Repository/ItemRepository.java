da564902a9bba5f17d18bb67357f3a2940c463c2
Nishanth <nishanth.badim@zemosolabs.com>
2020-05-28 10:49:28 +0530

    Linux Assignment

--- ShoppingCartApp/src/main/java/com/example/SpringApp/Repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SpringApp.Repository;
-
-import com.example.SpringApp.Entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-}
--- SpringBootAssignment/src/main/java/com/example/springsecuritylogin/Repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springsecuritylogin.Repository;
-
-import com.example.springsecuritylogin.Entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-}
