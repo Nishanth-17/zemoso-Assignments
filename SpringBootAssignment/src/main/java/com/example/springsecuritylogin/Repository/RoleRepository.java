1da80910a20425df11d48aa7e0647d160ac3354a
Nishanth <nishanth.badim@zemosolabs.com>
2020-06-12 21:27:34 +0530

    Added JS Assignment

--- SpringBootAssignment/src/main/java/com/example/springsecuritylogin/Repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springsecuritylogin.Repository;
-
-import com.example.springsecuritylogin.Entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
--- SpringBootAssignment/src/main/java/com/example/springsecuritylogin/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springsecuritylogin.Repository;
-
-import com.example.springsecuritylogin.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
