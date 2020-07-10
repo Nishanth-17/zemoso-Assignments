554af53ec6b34a432239f3b7468aa3ae997a21ec
Nishanth <nishanth.badim@zemosolabs.com>
2020-07-10 10:55:11 +0530

    Add Redux App

---
+++ Calculator-Redux/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calculator from './_components/pages/_calculator'
+import { ThemeProvider } from "@material-ui/core";
+import { myTheme } from "./theme";
+
+
+function App() {
+  return (
+    <ThemeProvider theme={myTheme}>
+      <Calculator />
+    </ThemeProvider>
+  );
+}
+
+export default App;
